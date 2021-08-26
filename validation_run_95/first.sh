9f9af21460cec7258b3beb23df490f91f301c36f
Lior Galanti <lior.galanti@gmail.com>
2021-08-26 14:10:55 -0400

    Update first.sh

--- validation_run_95/first.sh
+++ validation_run_95/first.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-time pheniqs mux --config first.json --input SRR6750041.cram --input SRR6750041.cram --report SRR6750041_first_report.json --prior SRR6750041_first_adjusted.json --output /dev/null
-time pheniqs mux --config first.json --input SRR6750056.cram --input SRR6750056.cram --report SRR6750056_first_report.json --prior SRR6750056_first_adjusted.json --output /dev/null
-time pheniqs mux --config first.json --input SRR6750057.cram --input SRR6750057.cram --report SRR6750057_first_report.json --prior SRR6750057_first_adjusted.json --output /dev/null
-time pheniqs mux --config first.json --input SRR6750058.cram --input SRR6750058.cram --report SRR6750058_first_report.json --prior SRR6750058_first_adjusted.json --output /dev/null
-time pheniqs mux --config first.json --input SRR6750059.cram --input SRR6750059.cram --report SRR6750059_first_report.json --prior SRR6750059_first_adjusted.json --output /dev/null
+time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/validation_run_95/first.json --base-output . --base-input ../raw_cram --input SRR6750041.cram --input SRR6750041.cram --report SRR6750041_first_report.json --prior SRR6750041_first_adjusted.json --output /dev/null
+time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/validation_run_95/first.json --base-output . --base-input ../raw_cram --input SRR6750056.cram --input SRR6750056.cram --report SRR6750056_first_report.json --prior SRR6750056_first_adjusted.json --output /dev/null
+time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/validation_run_95/first.json --base-output . --base-input ../raw_cram --input SRR6750057.cram --input SRR6750057.cram --report SRR6750057_first_report.json --prior SRR6750057_first_adjusted.json --output /dev/null
+time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/validation_run_95/first.json --base-output . --base-input ../raw_cram --input SRR6750058.cram --input SRR6750058.cram --report SRR6750058_first_report.json --prior SRR6750058_first_adjusted.json --output /dev/null
+time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/validation_run_95/first.json --base-output . --base-input ../raw_cram --input SRR6750059.cram --input SRR6750059.cram --report SRR6750059_first_report.json --prior SRR6750059_first_adjusted.json --output /dev/null
