33de91734ca709c32e957cea9635f3cc9ea08d1d
Lior Galanti <lior.galanti@gmail.com>
2021-08-26 16:26:58 -0400

    Create second.sh

---
+++ validation_run_95/second.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+time pheniqs mux --config SRR6750041_first_adjusted.json --report SRR6750041_second_report.json --prior SRR6750041_second_adjusted.json --output SRR6750041_decoded.cram
+time pheniqs mux --config SRR6750056_first_adjusted.json --report SRR6750056_second_report.json --prior SRR6750056_second_adjusted.json --output SRR6750056_decoded.cram
+time pheniqs mux --config SRR6750057_first_adjusted.json --report SRR6750057_second_report.json --prior SRR6750057_second_adjusted.json --output SRR6750057_decoded.cram
+time pheniqs mux --config SRR6750058_first_adjusted.json --report SRR6750058_second_report.json --prior SRR6750058_second_adjusted.json --output SRR6750058_decoded.cram
+time pheniqs mux --config SRR6750059_first_adjusted.json --report SRR6750059_second_report.json --prior SRR6750059_second_adjusted.json --output SRR6750059_decoded.cram
