b65ff23b9f8cfe3221b9c7b90c43c1c9157bc70e
Lior Galanti <lior.galanti@gmail.com>
2021-08-26 10:56:49 -0400

    add time benchmarks for first run

--- single_run/first.sh
+++ single_run/first.sh
@@ -19,3 +19,79 @@ time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/pheniqs_config/3
 time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/pheniqs_config/3_round.json --input SRR6750057.cram --input SRR6750057.cram --report SRR6750057_first_report.json --prior SRR6750057_adjusted.json --output SRR6750057_decoded.cram
 time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/pheniqs_config/3_round.json --input SRR6750058.cram --input SRR6750058.cram --report SRR6750058_first_report.json --prior SRR6750058_adjusted.json --output SRR6750058_decoded.cram
 time pheniqs mux --config ~/code/moonwatcher/pheniqs-split-pool/pheniqs_config/3_round.json --input SRR6750059.cram --input SRR6750059.cram --report SRR6750059_first_report.json --prior SRR6750059_adjusted.json --output SRR6750059_decoded.cram
+
+# real    6m17.643s
+# user    115m37.399s
+# sys     9m57.152s
+#
+# real    14m20.102s
+# user    266m5.827s
+# sys     22m28.088s
+#
+# real    13m44.226s
+# user    256m2.899s
+# sys     21m13.302s
+#
+# real    14m43.200s
+# user    273m42.761s
+# sys     22m58.733s
+#
+# real    13m15.220s
+# user    248m14.994s
+# sys     20m49.634s
+#
+# real    13m50.468s
+# user    256m59.135s
+# sys     21m21.438s
+#
+# real    13m4.805s
+# user    241m49.886s
+# sys     20m10.654s
+#
+# real    13m51.137s
+# user    258m11.673s
+# sys     21m44.755s
+#
+# real    12m51.308s
+# user    240m51.342s
+# sys     20m19.377s
+#
+# real    15m38.111s
+# user    293m9.080s
+# sys     24m20.434s
+#
+# real    14m12.661s
+# user    262m58.231s
+# sys     22m2.003s
+#
+# real    13m1.941s
+# user    242m12.174s
+# sys     20m13.421s
+#
+# real    18m36.980s
+# user    346m36.655s
+# sys     28m46.585s
+#
+# real    14m59.525s
+# user    282m54.296s
+# sys     23m27.793s
+#
+# real    13m47.966s
+# user    258m10.785s
+# sys     21m48.841s
+#
+# real    17m52.050s
+# user    330m6.469s
+# sys     27m20.325s
+#
+# real    17m28.989s
+# user    324m44.996s
+# sys     27m14.071s
+#
+# real    17m52.451s
+# user    335m50.940s
+# sys     27m48.172s
+#
+# real    19m27.052s
+# user    364m37.377s
+# sys     31m31.067s
