db72661e197bfd4e2cfb0db967a2ee1422de4003
Lior Galanti <lior.galanti@gmail.com>
2021-09-01 16:45:37 -0400

    redo the full run

---
+++ full_run_95/second.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+time pheniqs mux --config SRR6750041_first_adjusted.json --report SRR6750041_second_report.json --prior SRR6750041_second_adjusted.json --output SRR6750041_decoded.cram
+time pheniqs mux --config SRR6750042_first_adjusted.json --report SRR6750042_second_report.json --prior SRR6750042_second_adjusted.json --output SRR6750042_decoded.cram
+time pheniqs mux --config SRR6750043_first_adjusted.json --report SRR6750043_second_report.json --prior SRR6750043_second_adjusted.json --output SRR6750043_decoded.cram
+time pheniqs mux --config SRR6750044_first_adjusted.json --report SRR6750044_second_report.json --prior SRR6750044_second_adjusted.json --output SRR6750044_decoded.cram
+time pheniqs mux --config SRR6750045_first_adjusted.json --report SRR6750045_second_report.json --prior SRR6750045_second_adjusted.json --output SRR6750045_decoded.cram
+time pheniqs mux --config SRR6750046_first_adjusted.json --report SRR6750046_second_report.json --prior SRR6750046_second_adjusted.json --output SRR6750046_decoded.cram
+time pheniqs mux --config SRR6750047_first_adjusted.json --report SRR6750047_second_report.json --prior SRR6750047_second_adjusted.json --output SRR6750047_decoded.cram
+time pheniqs mux --config SRR6750048_first_adjusted.json --report SRR6750048_second_report.json --prior SRR6750048_second_adjusted.json --output SRR6750048_decoded.cram
+time pheniqs mux --config SRR6750049_first_adjusted.json --report SRR6750049_second_report.json --prior SRR6750049_second_adjusted.json --output SRR6750049_decoded.cram
+time pheniqs mux --config SRR6750050_first_adjusted.json --report SRR6750050_second_report.json --prior SRR6750050_second_adjusted.json --output SRR6750050_decoded.cram
+time pheniqs mux --config SRR6750051_first_adjusted.json --report SRR6750051_second_report.json --prior SRR6750051_second_adjusted.json --output SRR6750051_decoded.cram
+time pheniqs mux --config SRR6750052_first_adjusted.json --report SRR6750052_second_report.json --prior SRR6750052_second_adjusted.json --output SRR6750052_decoded.cram
+time pheniqs mux --config SRR6750053_first_adjusted.json --report SRR6750053_second_report.json --prior SRR6750053_second_adjusted.json --output SRR6750053_decoded.cram
+time pheniqs mux --config SRR6750054_first_adjusted.json --report SRR6750054_second_report.json --prior SRR6750054_second_adjusted.json --output SRR6750054_decoded.cram
+time pheniqs mux --config SRR6750055_first_adjusted.json --report SRR6750055_second_report.json --prior SRR6750055_second_adjusted.json --output SRR6750055_decoded.cram
+time pheniqs mux --config SRR6750056_first_adjusted.json --report SRR6750056_second_report.json --prior SRR6750056_second_adjusted.json --output SRR6750056_decoded.cram
+time pheniqs mux --config SRR6750057_first_adjusted.json --report SRR6750057_second_report.json --prior SRR6750057_second_adjusted.json --output SRR6750057_decoded.cram
+time pheniqs mux --config SRR6750058_first_adjusted.json --report SRR6750058_second_report.json --prior SRR6750058_second_adjusted.json --output SRR6750058_decoded.cram
+time pheniqs mux --config SRR6750059_first_adjusted.json --report SRR6750059_second_report.json --prior SRR6750059_second_adjusted.json --output SRR6750059_decoded.cram
